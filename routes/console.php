c5453a98200b94a5626e45fb84d7941976caa877
Jonathan Henrique <jhowjhoe@icloud.com>
2024-08-22 12:47:39 -0300

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -3,9 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
 
 //run php artisan app:send-messages-command
 Artisan::command('app:send-messages-command', function () {
