1f685fbe5e98939198ca8de6f79451d33197bae6
Jonathan Henrique <jhowjhoe@icloud.com>
2024-08-23 00:03:47 -0300

    fix: jobs run everyMinute

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-// Artisan::command('inspire', function () {
-//     $this->comment(Inspiring::quote());
-// })->purpose('Display an inspiring quote')->hourly();
-
-//run php artisan app:send-messages-command
-// Artisan::command('app:send-messages-command', function () {
-//     $this->info('Messages sent successfully!');
-// })->purpose('Send messages to users')->daily();
+Schedule::command('app:send-messages-command')->everyMinute();
