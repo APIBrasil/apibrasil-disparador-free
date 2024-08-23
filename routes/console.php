458764b813086af3a75c6a149cd62cfce86fa2a8
Jonathan Henrique <jhowjhoe@icloud.com>
2024-08-23 14:12:36 -0300

    fix: overlapping

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,7 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:send-messages-command')->everyMinute();
+//not overlapping  with the web routes
+Schedule::command('app:send-messages-command')->everyMinute()
+    // ->appendOutputTo(storage_path('logs/send-messages-command.log'))
+    ->withoutOverlapping();
